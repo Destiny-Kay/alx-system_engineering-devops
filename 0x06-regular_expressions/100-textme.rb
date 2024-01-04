c95f16bfdc536353ffe7cdaa1ce0a769dcc48319
Destiny-Kay <destinykevogo@gmail.com>
2024-01-04 11:35:11 +0300

    Added: extra files

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,1 +1,5 @@
 #!/usr/bin/env ruby
+sender = ARGV[0].scan(/(?<=from:)(\W?\w+)/).join
+receiver = ARGV[0].scan(/(?<=to:)\W?\w+/).join
+flags = ARGV[0].scan(/(?<=flags:)(.?\d..?\d..?\d..?\d..?\d)/).join
+puts("#{sender},#{receiver},#{flags}")
