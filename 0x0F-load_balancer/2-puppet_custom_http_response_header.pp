0b7db549ecbf257dffd4e67929b14b5ccce190fc
Destiny-Kay <destinykevogo@gmail.com>
2024-03-04 23:23:05 +0300

    Add puppet file

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# setting up nginx with a custom http header
+
+exec {'update system':
+    command => '/usr/bin/apt-get update',
+}
+
+package {'nginx':
+    ensure => 'installed',
+    require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+    content => 'Hello world!'
+}
+
+exec {'HTTP header':
+    command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+    provider => 'shell'
+}
+
+service {'nginx':
+    ensure=> running,
+    require => Package['nginx']
+}
